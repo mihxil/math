c66f8451f3b880b66a35cbf237b9bd4583afabdb
michiel <michiel.meeuwissen@vpro.nl>
2012-09-07 23:04:43 +0200

    moved to test-class

--- src/main/java/org/meeuw/math/Measurement.java
+++ src/main/java/org/meeuw/math/Measurement.java
@@ -300,31 +300,6 @@ public class Measurement extends java.lang.Number {
              : "");
     }
 
-    public static void main(String[] argv) {
-        if (argv.length == 0) {
-            test();
-        } else {
-            Measurement measurement = new Measurement();
-            for (String arg : argv) {
-                measurement.enter(Double.valueOf(arg));
-            }
-            System.out.println(measurement);
-        }
-
-    }
-
-    public static void test() {
-        System.out.println(new Measurement().enter(-10, 20));
-        System.out.println(new Measurement().enter(0, 0));
-        System.out.println(new Measurement().enter(0, 1));
-        System.out.println(new Measurement().enter(-0.001, -0.002));
-        System.out.println(new Measurement().enter(0.5, 1.5));
-        System.out.println(new Measurement().enter(20000, 20001, 20002, 20003));
-        System.out.println(new Measurement().enter(20000, 20010, 20020, 20030));
-        System.out.println(new Measurement().enter(20000, 20100, 20200, 20300));
-        System.out.println(new Measurement().enter(0.000002, 0.0000021, 0.0000022, 0.0000023));
-        System.out.println(new Measurement().enter(0.0000002, 0.000000201, 0.000000202, 0.000000203));
-    }
 }
 
 
