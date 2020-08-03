7d2f3bb42dc8b389fe4d60a46634d8a84dcee9b7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-08-03 08:27:45 +0200

    Details

--- mihxil-algebra/src/test/java/org/meeuw/math/abstractalgebra/dim3/RotationTest.java
+++ mihxil-algebra/src/test/java/org/meeuw/math/abstractalgebra/dim3/RotationTest.java
@@ -1,8 +1,13 @@
 package org.meeuw.math.abstractalgebra.dim3;
 
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+
 import org.junit.jupiter.api.Test;
+import org.meeuw.math.abstractalgebra.MultiplicativeGroupTheory;
 import org.meeuw.math.abstractalgebra.reals.RealNumber;
 
+import static java.lang.Math.PI;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.meeuw.math.abstractalgebra.dim3.FieldVector3.of;
 
@@ -10,11 +15,11 @@ import static org.meeuw.math.abstractalgebra.dim3.FieldVector3.of;
  * @author Michiel Meeuwissen
  * @since 0.4
  */
-class RotationTest {
+class RotationTest implements MultiplicativeGroupTheory<Rotation> {
 
     @Test
     public void roty() {
-        Rotation y = Rotation.Ry(Math.PI);
+        Rotation y = Rotation.Ry(PI);
         FieldVector3<RealNumber> v = of(1, 0, 0);
         FieldVector3<RealNumber> rotated = y.rotate(v);
         assertThat(rotated).isEqualTo(of(-1, 0, 0));
@@ -22,11 +27,15 @@ class RotationTest {
 
      @Test
     public void rotz() {
-        Rotation z = Rotation.Rz(Math.PI);
+        Rotation z = Rotation.Rz(PI);
         FieldVector3<RealNumber> v = of(0, 1, 0);
         FieldVector3<RealNumber> rotated = z.rotate(v);
         assertThat(rotated).isEqualTo(of(0, -1, 0));
     }
 
 
+    @Override
+    public Arbitrary<Rotation> elements() {
+        return Arbitraries.of(Rotation.Ry(PI / 2), Rotation.Rz(PI / 3), Rotation.Ry(PI / 6).times(Rotation.Rx(PI / 9)));
+    }
 }
