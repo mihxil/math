9e11143354a4d60788deb79e01ff08c8e85dff90
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-08-05 23:20:35 +0200

    More testing.

--- mihxil-math/src/test/java/org/meeuw/math/abstractalgebra/ElementTheory.java
+++ mihxil-math/src/test/java/org/meeuw/math/abstractalgebra/ElementTheory.java
@@ -2,6 +2,8 @@ package org.meeuw.math.abstractalgebra;
 
 import net.jqwik.api.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @author Michiel Meeuwissen
  * @since 0.4
@@ -17,5 +19,11 @@ public interface ElementTheory<E>  {
     @Provide
     default Arbitrary<E> element() {
         return Arbitraries.of(elements().sample());
+    }
+
+    @Property
+    default void testEquals(@ForAll(ELEMENTS) E e) {
+        assertThat(e).isEqualTo(e);
+        assertThat(e).isNotEqualTo(new Object());
     }
 }
