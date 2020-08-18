8d929de0f3b2fdaaa1788918fdc7067bff36e51a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-08-18 15:07:40 +0200

    More testing.

---
+++ mihxil-math/src/test/java/org/meeuw/math/abstractalgebra/CardinalityTest.java
@@ -0,0 +1,50 @@
+package org.meeuw.math.abstractalgebra;
+
+import net.jqwik.api.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 0.4
+ */
+class CardinalityTest {
+
+    @Test
+    public void alephs() {
+        assertThat(Cardinality.ALEPH_0).isGreaterThan(new Cardinality(Long.MAX_VALUE));
+        assertThat(Cardinality.ALEPH_1).isGreaterThan(Cardinality.ALEPH_0);
+        assertThat(Cardinality.ALEPH_0.toString()).isEqualTo("א\u200E₀");
+        assertThat(Cardinality.ALEPH_1.toString()).isEqualTo("א\u200E₁");
+    }
+    @Test
+    public void test() {
+        assertThat(new Cardinality(10)).isGreaterThan(new Cardinality(9));
+        assertThat(new Cardinality(9)).isLessThan(new Cardinality(10));
+        assertThat(new Cardinality(9)).isLessThan(Cardinality.ALEPH_0);
+
+        assertThat(new Cardinality(10)).isEqualTo(new Cardinality(10));
+        assertThat(new Cardinality(10).toString()).isEqualTo("10");
+        assertThat(new Cardinality(10).getValue()).isEqualTo(10);
+
+    }
+
+    @Property
+    public void hash(@ForAll("cardinalities") Cardinality c1, @ForAll("cardinalities") Cardinality c2) {
+        if (c1.equals(c2)) {
+            assertThat(c2).isEqualTo(c1);
+            assertThat(c1.hashCode()).isEqualTo(c2.hashCode());
+        }
+        assertThat(c1.compareTo(c2)).isEqualTo(-1 * c2.compareTo(c1));
+    }
+
+
+
+    @Provide
+    public Arbitrary<Cardinality> cardinalities() {
+        return Arbitraries.of(Cardinality.ALEPH_0, Cardinality.ALEPH_1, new Cardinality(0), new Cardinality(9), new Cardinality(10), new Cardinality(Long.MAX_VALUE));
+    }
+
+}
