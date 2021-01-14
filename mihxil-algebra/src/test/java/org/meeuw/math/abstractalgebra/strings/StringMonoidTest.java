4e0858dbde8adc010a7798268d1c5b1119f577c1
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-01-14 15:19:54 +0100

    cover also those last lines

--- mihxil-algebra/src/test/java/org/meeuw/math/abstractalgebra/strings/StringMonoidTest.java
+++ mihxil-algebra/src/test/java/org/meeuw/math/abstractalgebra/strings/StringMonoidTest.java
@@ -2,19 +2,57 @@ package org.meeuw.math.abstractalgebra.strings;
 
 import net.jqwik.api.*;
 
+import org.junit.jupiter.api.Test;
 import org.meeuw.math.abstractalgebra.test.AdditiveMonoidTheory;
 import org.meeuw.util.test.CharSequenceTheory;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Michiel Meeuwissen
  * @since 0.4
  */
-class StringMonoidTest implements AdditiveMonoidTheory<StringElement>, CharSequenceTheory<StringElement> {
+class StringMonoidTest implements
+    AdditiveMonoidTheory<StringElement>,
+    CharSequenceTheory<StringElement> {
+
+    @Test
+    public void stream1() {
+        assertThat(StringMonoid.INSTANCE.stream(new StringMonoid.State('a', 'x')).limit(10)).map(StringElement::toString)
+            .containsExactly(
+                "ax", "ay", "az", "a ", "aª", "aµ", "aº", "aÀ", "aÁ", "aÂ"
+            );
+    }
+    @Test
+    public void stream2() {
+        assertThat(StringMonoid.INSTANCE.stream(new StringMonoid.State('a', StringMonoid.LAST_CHAR - 1)).limit(10)).map(StringElement::toString)
+            .containsExactly(
+                "a鼻", "a𪘀", "b ", "b0", "b1", "b2", "b3", "b4", "b5", "b6"
+            );
+    }
+    @Test
+    public void stream3() {
+        assertThat(StringMonoid.INSTANCE.stream(new StringMonoid.State('a', StringMonoid.LAST_CHAR, StringMonoid.LAST_CHAR - 1)).limit(10)).map(StringElement::toString)
+            .containsExactly(
+                "a𪘀鼻",
+                "a𪘀𪘀",
+                "b𪘀 ",
+                "b𪘀0",
+                "b𪘀1",
+                "b𪘀2",
+                "b𪘀3",
+                "b𪘀4",
+                "b𪘀5",
+                "b𪘀6"
+            );
+
+    }
 
     @Override
     @Provide
     public Arbitrary<StringElement> elements() {
-        return Arbitraries.of("a", "foo", "bar", "").map(StringElement::new);
+        return Arbitraries.of("a", "foo", "bar", "")
+            .map(StringElement::new);
     }
 
 
