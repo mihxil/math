14f3c309fb69e000d6a37a945cb9cd920d1a9f19
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-03-17 19:45:17 +0100

    testing

--- mihxil-physics/src/test/java/org/meeuw/physics/SITest.java
+++ mihxil-physics/src/test/java/org/meeuw/physics/SITest.java
@@ -1,20 +1,29 @@
 package org.meeuw.physics;
 
+import lombok.extern.log4j.Log4j2;
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Michiel Meeuwissen
  * @since 0.4
  */
+@Log4j2
 class SITest {
 
     @Test
     public void ly() {
         assertThat(SI.ly.toString()).isEqualTo("ly");
         assertThat(SI.ly.toSI().toString()).isEqualTo("9.4607304725808·10¹⁵ m");
+    }
+
+    @Test
+    public void test() {
+        for (Prefix p : SI.Prefixes.values()) {
+            log.info("{}: {} ", p, p.getAsDouble());
+        }
     }
 
 
