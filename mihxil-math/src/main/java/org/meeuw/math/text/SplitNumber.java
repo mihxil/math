89e51fdfdbd6a5569af2c6eec28aa7fe93d4fa77
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2021-04-02 23:09:15 +0200

    infinity simply cannot be split

--- mihxil-math/src/main/java/org/meeuw/math/text/SplitNumber.java
+++ mihxil-math/src/main/java/org/meeuw/math/text/SplitNumber.java
@@ -1,7 +1,5 @@
 package org.meeuw.math.text;
 
-import org.meeuw.math.Utils;
-
 /**
  * Split a double up in 2 numbers: a double approximately 1 (the 'coefficient'), and an integer
  * indicating the order of magnitude (the 'exponent').
@@ -16,7 +14,6 @@ class SplitNumber {
         this.exponent = exponent;
     }
 
-
     @Override
     public String toString() {
         return coefficient + UncertainDoubleFormat.TIMES_10 + TextUtils.superscript(exponent);
@@ -24,7 +21,7 @@ class SplitNumber {
 
     static SplitNumber split(double in) {
         if (Double.isInfinite(in)) {
-            return new SplitNumber(in == Double.NEGATIVE_INFINITY ? -1 : 1, Utils.log10(Integer.MAX_VALUE));
+            throw new IllegalArgumentException("" + in);
         }
         boolean negative = in < 0;
         double coefficient = Math.abs(in);
