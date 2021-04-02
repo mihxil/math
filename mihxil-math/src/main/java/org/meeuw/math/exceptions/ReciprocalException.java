80c14b1640d3e02ec45b2d1774fdd6f2a13ce8be
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2021-04-02 23:09:25 +0200

    javadoc

--- mihxil-math/src/main/java/org/meeuw/math/exceptions/ReciprocalException.java
+++ mihxil-math/src/main/java/org/meeuw/math/exceptions/ReciprocalException.java
@@ -1,6 +1,11 @@
 package org.meeuw.math.exceptions;
 
+import org.meeuw.math.abstractalgebra.MultiplicativeGroupElement;
+
 /**
+ * Represents a problem with taking the reciprocal of an element. The must well known extension would be {@link DivisionByZeroException}.
+ * But it is also thrown by {@link MultiplicativeGroupElement#reciprocal()} if this is exceptionaly not possible.
+ *
  * @author Michiel Meeuwissen
  * @since 0.4
  */
