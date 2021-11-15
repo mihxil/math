76a5b5f8235dd284a07711e9f4c6504aee59855c
Michiel Meeuwissen <git@meeuw.org>
2021-11-15 16:47:15 +0100

    Just added some tests.

--- report-aggregation/src/test/java/org/meeuw/test/IntegrationTest.java
+++ report-aggregation/src/test/java/org/meeuw/test/IntegrationTest.java
@@ -3,6 +3,7 @@ package org.meeuw.test;
 import org.junit.jupiter.api.Test;
 
 import org.meeuw.math.TestClock;
+import org.meeuw.math.statistics.StatisticalDouble;
 import org.meeuw.math.windowed.WindowedEventRate;
 import org.meeuw.physics.*;
 
@@ -14,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class IntegrationTest {
 
     @Test
-    public void test() {
+    public void windowedEventRateToPhysics() {
         TestClock clock = new TestClock();
         WindowedEventRate rate = WindowedEventRate.builder()
             .clock(clock)
@@ -28,5 +29,16 @@ public class IntegrationTest {
         PhysicalNumber rateInHours = measurement.toUnits(Units.of(SI.hour).reciprocal());
         assertThat(rateInHours.toString()).isEqualTo("7200 h⁻¹");
         System.out.println("Rate: " + rateInHours);
+    }
+
+    @Test
+    public void statisticalDoubleToPhysics() {
+
+        StatisticalDouble statisticalDouble = new StatisticalDouble();
+        statisticalDouble.enter(10d, 11d, 9d);
+
+        PhysicalNumber measurement = new Measurement(statisticalDouble, Units.of(SI.min));
+
+        assertThat(measurement.toUnits(Units.of(SIUnit.s)).toString()).isEqualTo("600 ± 45 s");
     }
 }
