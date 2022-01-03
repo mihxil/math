5a881fdc7b64afc81f8087e8cb040becbcb61fb1
Michiel Meeuwissen <git@meeuw.org>
2022-01-03 08:38:53 +0100

    More module infos

---
+++ mihxil-physics/src/main/java/module-info.java
@@ -0,0 +1,26 @@
+import org.meeuw.math.text.spi.AlgebraicElementFormatProvider;
+import org.meeuw.physics.text.spi.*;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 0.4
+ */
+module org.meeuw.physics {
+    requires static lombok;
+    requires org.meeuw.math;
+    requires java.validation;
+    requires org.checkerframework.checker.qual;
+    requires org.meeuw.configuration;
+
+    exports org.meeuw.physics;
+
+    uses AlgebraicElementFormatProvider;
+
+    provides AlgebraicElementFormatProvider with
+        DimensionsFormatProvider,
+        PhysicalNumberFormatProvider,
+        UnitsFormatProvider
+        ;
+
+}
+
---
+++ mihxil-statistics/src/main/java/module-info.java
@@ -0,0 +1,33 @@
+import org.meeuw.configuration.ConfigurationAspect;
+import org.meeuw.math.statistics.text.TimeConfiguration;
+import org.meeuw.math.statistics.text.spi.StatisticalLongNumberFormatProvider;
+import org.meeuw.math.text.spi.AlgebraicElementFormatProvider;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 0.4
+ */
+module org.meeuw.math.statistics {
+    requires static lombok;
+    requires java.logging;
+    requires org.meeuw.math;
+    requires java.validation;
+    requires org.meeuw.configuration;
+
+    exports org.meeuw.math.statistics;
+    exports org.meeuw.math.statistics.text;
+
+
+    uses AlgebraicElementFormatProvider;
+    uses ConfigurationAspect;
+
+
+    provides AlgebraicElementFormatProvider with
+        StatisticalLongNumberFormatProvider;
+
+    provides ConfigurationAspect with
+        TimeConfiguration;
+
+
+}
+
