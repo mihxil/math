41880e6f191f4e173f0a3f470a690848c33ecbd9
Michiel Meeuwissen <git@meeuw.org>
2022-04-12 22:27:24 +0200

    Added copyright information.

--- mihxil-math/src/main/java/org/meeuw/math/abstractalgebra/AdditiveAbelianGroup.java
+++ mihxil-math/src/main/java/org/meeuw/math/abstractalgebra/AdditiveAbelianGroup.java
@@ -1,12 +1,26 @@
+/*
+ *  Copyright 2022 Michiel Meeuwissen
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.meeuw.math.abstractalgebra;
 
 import java.util.NavigableSet;
 
-import static org.meeuw.math.CollectionUtils.navigableSet;
-
 import org.meeuw.math.operators.AlgebraicBinaryOperator;
 import org.meeuw.math.operators.AlgebraicUnaryOperator;
 
+import static org.meeuw.math.CollectionUtils.navigableSet;
 import static org.meeuw.math.operators.BasicAlgebraicBinaryOperator.SUBTRACTION;
 import static org.meeuw.math.operators.BasicAlgebraicUnaryOperator.NEGATION;
 
