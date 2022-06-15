db75c28de8a9d2ace3f31a4984e5b429f55ab283
Michiel Meeuwissen <git@meeuw.org>
2022-06-15 12:15:18 +0200

    javadoc.

--- mihxil-algebra/src/main/java/org/meeuw/math/abstractalgebra/klein/package-info.java
+++ mihxil-algebra/src/main/java/org/meeuw/math/abstractalgebra/klein/package-info.java
@@ -1,4 +1,50 @@
 /**
  * <a href="https://en.wikipedia.org/wiki/Klein_four-group">The Klein four-group</a>
+ *
+ *
+ *<table>
+ * <tbody><tr>
+ * <th>*
+ * </th>
+ * <th>e
+ * </th>
+ * <th><i>a</i>
+ * </th>
+ * <th><i>b</i>
+ * </th>
+ * <th><i>c</i>
+ * </th></tr>
+ * <tr align="center">
+ * <th>e
+ * </th>
+ * <td>e</td>
+ * <td><i>a</i></td>
+ * <td><i>b</i></td>
+ * <td><i>c</i>
+ * </td></tr>
+ * <tr align="center">
+ * <th><i>a</i>
+ * </th>
+ * <td><i>a</i></td>
+ * <td>e</td>
+ * <td><i>c</i></td>
+ * <td><i>b</i>
+ * </td></tr>
+ * <tr align="center">
+ * <th><i>b</i>
+ * </th>
+ * <td><i>b</i></td>
+ * <td><i>c</i></td>
+ * <td>e</td>
+ * <td><i>a</i>
+ * </td></tr>
+ * <tr align="center">
+ * <th><i>c</i>
+ * </th>
+ * <td><i>c</i></td>
+ * <td><i>b</i></td>
+ * <td><i>a</i></td>
+ * <td>e
+ * </td></tr></tbody></table>
  */
 package org.meeuw.math.abstractalgebra.klein;
