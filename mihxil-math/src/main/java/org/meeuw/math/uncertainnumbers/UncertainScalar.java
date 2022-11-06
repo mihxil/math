6b60a2746f688b857bc21cd2d6e3fa7d755d4bbf
Michiel Meeuwissen <git@meeuw.org>
2022-11-06 21:35:56 +0100

    Introduced an interface for uncertain numbers that are scalars too

---
+++ mihxil-math/src/main/java/org/meeuw/math/uncertainnumbers/UncertainScalar.java
@@ -0,0 +1,16 @@
+package org.meeuw.math.uncertainnumbers;
+
+import org.meeuw.math.numbers.Scalar;
+
+/**
+ * A {@link UncertainNumber} that is a {@link Scalar} too.
+ * @since 0.9
+ */
+public interface UncertainScalar<N extends Number, SELF extends UncertainScalar<N, SELF>>
+    extends  UncertainNumber<N>, Scalar<SELF> {
+
+    @Override
+    default int signum() {
+        return (int) Math.signum(doubleValue());
+    }
+}
