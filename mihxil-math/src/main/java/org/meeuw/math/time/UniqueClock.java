cb2080a43662888b6dc37e8c0908f8b424ceaea3
Michiel Meeuwissen <git@meeuw.org>
2022-12-08 20:40:37 +0100

    javadoc.

--- mihxil-math/src/main/java/org/meeuw/math/time/UniqueClock.java
+++ mihxil-math/src/main/java/org/meeuw/math/time/UniqueClock.java
@@ -3,9 +3,9 @@ package org.meeuw.math.time;
 import java.time.*;
 
 /**
- * A clock that ticks uniquely. Every call to {@link #instant()} will be a time after the previous call to it.
+ * A clock that ticks uniquely. Every call to {@link #instant()} will result an {@link Instant} at least 1 nanosecond after the previous call to it.
  * <p>
- * If no nanosecond has passed since the previous call, an extra nanosecond will be implicitly added.
+ * If no nanosecond has passed since the previous call, an extra nanosecond will be implicitly added, so {@link #instant()} may actually be bit in the future.
  *
  * @author Michiel Meeuwissen
  * @since 0.10
@@ -31,6 +31,10 @@ public class UniqueClock extends Clock {
         return result;
     }
 
+    /**
+     * The current time according to this {@link UniqueClock}. Normally just the same
+     * as the instant returned by the wrapped base clock. Unless that would have been returned previously, in which an instant  1 (or more if needed) nanoseconds later will be produced (so, slightly in the future according to the base clock)
+     */
     @Override
     public synchronized Instant instant() {
         Instant answer = baseClock.instant();
