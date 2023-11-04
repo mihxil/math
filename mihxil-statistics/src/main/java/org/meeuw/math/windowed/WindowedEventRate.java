fce347f6ba0f6f4aa075965908b32bf9bd45cb85
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2023-11-04 20:08:21 +0100

    No point in using up 5, mostly idle, threads.

--- mihxil-statistics/src/main/java/org/meeuw/math/windowed/WindowedEventRate.java
+++ mihxil-statistics/src/main/java/org/meeuw/math/windowed/WindowedEventRate.java
@@ -58,7 +58,7 @@ public class WindowedEventRate extends Windowed<AtomicLong>
 
     private static final ThreadGroup THREAD_GROUP = new ThreadGroup("mihxil-statistics");
     private static final ScheduledExecutorService backgroundExecutor = Executors
-        .newScheduledThreadPool(5, new ThreadFactory() {
+        .newScheduledThreadPool(1, new ThreadFactory() {
             int counter = 0;
             @Override
             public Thread newThread(Runnable r) {
