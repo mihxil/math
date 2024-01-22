87cbf987ad856aaab0f404c0cf3500336a54a420
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-01-22 22:12:23 +0100

    Added a utility.

--- mihxil-math/src/test/java/org/meeuw/test/math/CollectionUtilsTest.java
+++ mihxil-math/src/test/java/org/meeuw/test/math/CollectionUtilsTest.java
@@ -15,15 +15,19 @@
  */
 package org.meeuw.test.math;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.SneakyThrows;
+
+import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
-import lombok.SneakyThrows;
-import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+
 import org.meeuw.math.CollectionUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CollectionUtilsTest {
 
@@ -40,20 +44,40 @@ public class CollectionUtilsTest {
         };
 
         Supplier<Integer> memoize = CollectionUtils.memoize(sup);
-        ExecutorService executor = Executors.newFixedThreadPool(10);
+        try (ExecutorService executor = Executors.newFixedThreadPool(10)) {
 
-        List<Future<Integer>> f = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            f.add(executor.submit(new Callable<Integer>() {
-                @Override
-                public Integer call() {
-                    return memoize.get();
-                }
-            }));
+            List<Future<Integer>> f = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                f.add(executor.submit(new Callable<Integer>() {
+                    @Override
+                    public Integer call() {
+                        return memoize.get();
+                    }
+                }));
+            }
+            for (Future<Integer> fut : f) {
+                assertThat(fut.get()).isEqualTo(1);
+            }
         }
-        for (Future<Integer> fut : f) {
-            assertThat(fut.get()).isEqualTo(1);
-        }
-        executor.shutdown();
     }
+
+
+    @Test
+    void nullSafeSet() {
+        Set<String> set = Set.of("a", "b");
+
+        assertThatThrownBy(() -> set.contains(null)).isInstanceOf(NullPointerException.class);
+
+        Set<String> nullSafeSet = CollectionUtils.nullSafeSet(set);
+
+        assertThat(nullSafeSet.contains(null)).isFalse();
+        assertThat(nullSafeSet.contains("a")).isTrue();
+
+        assertThat(nullSafeSet.size()).isEqualTo(2);
+
+        assertThatThrownBy(() -> nullSafeSet.add(null)).isInstanceOf(UnsupportedOperationException.class);
+
+        assertThat(nullSafeSet.iterator()).asList().containsExactly("a", "b");
+    }
+
 }
