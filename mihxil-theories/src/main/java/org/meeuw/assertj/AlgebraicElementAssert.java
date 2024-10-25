756c30dd5e7247c3c6f68beb31c49e1d8da02d06
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-10-25 17:17:39 +0200

    Better super class.

--- mihxil-theories/src/main/java/org/meeuw/assertj/AlgebraicElementAssert.java
+++ mihxil-theories/src/main/java/org/meeuw/assertj/AlgebraicElementAssert.java
@@ -1,19 +1,19 @@
 package org.meeuw.assertj;
 
-import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.AbstractObjectAssert;
 
 import org.meeuw.math.abstractalgebra.AlgebraicElement;
 
-public class AlgebraicElementAssert<E extends AlgebraicElement<E>> extends AbstractAssert<AlgebraicElementAssert<E>, E> {
+public class AlgebraicElementAssert<E extends AlgebraicElement<E>> extends AbstractObjectAssert<AlgebraicElementAssert<E>, E> {
 
     protected AlgebraicElementAssert(E o) {
         super(o, AlgebraicElementAssert.class);
     }
 
     @SuppressWarnings("UnusedReturnValue")
-    public AlgebraicElementAssert<E> isEqTo(E other) {
-        if (!actual.eq(other)) {
-            assertionError(actual + " ≉ " + other);
+    public AlgebraicElementAssert<E> isEqTo(E expected) {
+        if (!actual.eq(expected)) {
+            assertionError(actual + " ≉ " + expected + "(expected)");
         }
         return myself;
     }
