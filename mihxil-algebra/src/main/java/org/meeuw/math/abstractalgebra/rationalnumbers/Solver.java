f44b45530183043905ea804c1dbc0623145d5bd3
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-07-20 21:11:30 +0200

    A bit smarter

--- mihxil-algebra/src/main/java/org/meeuw/math/abstractalgebra/rationalnumbers/Solver.java
+++ mihxil-algebra/src/main/java/org/meeuw/math/abstractalgebra/rationalnumbers/Solver.java
@@ -4,32 +4,40 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
 
+import org.meeuw.math.CollectionUtils;
 import org.meeuw.math.abstractalgebra.permutations.PermutationGroup;
 import org.meeuw.math.arithmetic.ast.*;
 import org.meeuw.math.exceptions.MathException;
+import org.meeuw.math.operators.AlgebraicBinaryOperator;
+
+import static org.meeuw.math.operators.BasicAlgebraicBinaryOperator.*;
 
 /**
  * A tool to evaluate all possible expressions (of a certain number of rational numbers) (and check if it equals a certain value)
  */
 public  class Solver {
 
-    AtomicLong tries = new AtomicLong();
+    private static final NavigableSet<AlgebraicBinaryOperator> OPERATORS = CollectionUtils.navigableSet(
+        ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION
+    );
+
+    private final AtomicLong tries = new AtomicLong();
 
     public Stream<Expression<RationalNumber>> stream(RationalNumber... set) {
         PermutationGroup permutations = PermutationGroup.ofDegree(set.length);
 
         return permutations.stream()
-            .flatMap(permutation -> {
-                RationalNumber[] permuted = permutation.permute(set);
-                return AST.stream(
-                    List.of(permuted),
-                    RationalNumbers.INSTANCE.getSupportedOperators()
-                );
-            })
+            .map(permutation -> permutation.permute(set))
+            .map(List::of)
             .distinct()
-            .peek(e -> {
-                tries.getAndIncrement();
-            });
+            .flatMap(permuted ->
+                AST.stream(
+                    permuted,
+                    OPERATORS
+                ))
+            .map( e -> e.canonize(RationalNumbers.INSTANCE))
+            .distinct()
+            .peek(e -> tries.getAndIncrement());
     }
 
 
@@ -59,14 +67,16 @@ public  class Solver {
             set[i] = instance.parse(numbers[i]);
         }
         Solver solver = new Solver();
+        AtomicLong matches = new AtomicLong();
         return new SolverResult(solver.evaledStream(set)
             .filter(e ->
                 e.result().eq(result)
-            ).map(EvaluatedExpression::toString),
-            solver.tries);
+            ).peek(e -> matches.getAndIncrement())
+            .map(EvaluatedExpression::toString),
+            solver.tries, matches);
     }
 
-    public record SolverResult(Stream<String> stream, AtomicLong tries) {
+    public record SolverResult(Stream<String> stream, AtomicLong tries, AtomicLong matches) {
 
     }
 
@@ -78,7 +88,6 @@ public  class Solver {
         }
         SolverResult result = result(integers[0], Arrays.copyOfRange(integers, 1, integers.length));
         result.stream().forEach(System.out::println);
-        System.out.println("ready tried " + result.tries.get());
-
+        System.out.println("ready found " + result.matches().get() + ", tried " + result.tries.get() );
     }
 }
