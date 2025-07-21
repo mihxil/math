d8e3fb2efd211ddb77eca9aeff9ecad1b2196457
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-07-21 15:07:13 +0200

    Made it a supplier

--- mihxil-time/src/main/java/org/meeuw/time/dateparser/DynamicDateTime.java
+++ mihxil-time/src/main/java/org/meeuw/time/dateparser/DynamicDateTime.java
@@ -1,20 +1,40 @@
 package org.meeuw.time.dateparser;
 
+import lombok.Getter;
+import lombok.With;
+
 import java.io.StringReader;
 import java.time.*;
 import java.time.chrono.Chronology;
 import java.time.temporal.WeekFields;
-import java.util.*;
+import java.util.Locale;
+import java.util.Random;
 
-import lombok.Setter;
+import org.meeuw.functional.ThrowingFunction;
+import org.meeuw.functional.ThrowingSupplier;
 
-public class DynamicDateTime {
 
-    final Random random = new Random();
+public class DynamicDateTime implements ThrowingFunction<String, ZonedDateTime, ParseException> {
 
-    @Setter
-    private Clock clock = Clock.systemDefaultZone();
-    private Locale locale = Locale.getDefault();
+    @With
+    final Random random;
+
+    @Getter
+    @With
+    Clock clock;
+
+    @With
+    final Locale locale;
+
+    public DynamicDateTime() {
+        this(null, null, null);
+    }
+    @lombok.Builder
+    private DynamicDateTime(Random random, Clock clock, Locale locale) {
+        this.random = random == null ? new Random() : random;
+        this.clock = clock == null ? Clock.systemDefaultZone() : clock;
+        this.locale = locale == null ? Locale.getDefault() : locale;
+    }
 
     protected DayOfWeek getFirstDayOfWeek() {
         return WeekFields.of(locale).getFirstDayOfWeek();
@@ -34,16 +54,25 @@ public class DynamicDateTime {
         return Instant.ofEpochMilli(0).atZone(clock.getZone());
     }
 
-    public ZonedDateTime parse(String string) throws ParseException {
+    @Override
+    public ZonedDateTime applyWithException(String string) throws ParseException {
         DateParser parser = new DateParser(new StringReader(string));
         parser.setDynamicDateTime(this);
         parser.start();
         return parser.get();
     }
+    public ThrowingSupplier<ZonedDateTime, ParseException> supply(String string)  {
+        return () -> {
+            DateParser parser = new DateParser(new StringReader(string));
+            parser.setDynamicDateTime(this);
+            parser.start();
+            return parser.get();
+        };
+    }
 
     public static void main(String[] argv) throws ParseException {
         DynamicDateTime parser = new DynamicDateTime();
-        System.out.println("" + parser.parse(argv[1]));
+        System.out.println("" + parser.applyWithException(argv[1]));
     }
 
 }
