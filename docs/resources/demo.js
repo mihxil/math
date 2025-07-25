ddadadd399a5189dc989b08eb47403b64ae4145f
mihxil <mihxil@users.noreply.github.com>
2025-07-25 21:48:18 +0000

    Automated update of documentation https://mihxil.github.io/math/

--- docs/resources/demo.js
+++ docs/resources/demo.js
@@ -33,33 +33,75 @@ async function setupSolver() {
     const button = form.querySelector('button');
     const buttonText = button.textContent;
     const output = form.querySelector('output');
+    const result = form.querySelector("#solver_result");
+    const numbers = form.querySelector("#solver_numbers");
+    function go() {
+        button.textContent = buttonText;
+        button.disabled = false;
+    }
+
+
+    const model = {
+        ParseResult: null,
+        result: null,
+        parseIfNeeded: async function() {
+          if (this.result == null) {
+              await this.parse();
+          }
+          return this.result;
+        },
+
+        parse: async function() {
+            const splitNumbers = numbers.value.split(" ");
+            if (this.ParseResult == null) {
+                this.ParseResult = await setupFormWithClass(button, 'org.meeuw.math.test.Solver$ParseResult');
+            }
+            this.result = await this.ParseResult.parse(result.value, splitNumbers);
+            go();
+        },
+        parseResult:  async function(input) {
+            await this.parse();
+            return await this.result.resultError();
+        },
+        parseInput :  async function(input) {
+            await this.parse()
+            return await this.result.inputError();
+        },
+        reset: function() {
+            this.result =null;
+        }
+    };
+    result['model'] = model;
+    numbers['model'] = model;
+
 
     let Solver = null;
     form.onsubmit = async (e) => {
         e.preventDefault();
         Solver = await setupFormWithClass(button, 'org.meeuw.math.test.Solver');
-        await cj.org.meeuw.math.abstractalgebra.rationalnumbers.RationalNumbers
 
-        const result = form.querySelector("#solver_result").value;
-        const numbers = form.querySelector("#solver_numbers").value.split(" ");
         output.value = '';
         button.textContent = "executing..";
         try {
-            const solverResult = await Solver.result(result, numbers);
+            const parseResult = await model.parseIfNeeded();
+            output.value += "using: " + await (await parseResult.field()).toString();
+            const solverResult = await Solver.solve(parseResult);
+
             const stream = await solverResult.stream();
             const lines = await stream.toArray();
             for (let i = 0; i < lines.length; i++) {
-                output.value += await lines[i].toString() + "\n";
+                output.value += "\n" + await lines[i].toString();
             }
             const tries = await (await solverResult.tries()).get();
             const matches = await (await solverResult.matches()).get();
             output.value += `\nFound: ${matches}`;
             output.value += `\nTried: ${tries}`;
+
         } catch (error) {
             output.value += await error.toString();
         }
-        button.textContent = buttonText;
-        button.disabled = false;
+        go();
+
     };
 }
 
@@ -77,6 +119,8 @@ async function setupDynamicDate() {
     const button = form.querySelector('button');
     const buttonText = button.textContent;
     const output = form.querySelector('output');
+
+
 
     let DynamicDateTime = null;
     form.onsubmit = async (e) => {
@@ -115,13 +159,34 @@ async function setupValidation() {
                 input.classList.remove("invalid");
             }
         });
-    });
+     });
+     document.querySelectorAll(".demo input[data-parser]").forEach(input => {
 
+        input.addEventListener("input", async function (e) {
+            const input = e.target;
+            const parser = input.getAttribute("data-parser");
+            const model = input['model'];
+            model.reset();
+
+            const error = await model[parser](input.value);
+            if (error !== null) {
+                const errorMessage = input.getAttribute("data-error-message") || "Invalid input";
+
+                input.setCustomValidity(errorMessage);
+                // Optional: add visual feedback
+                input.classList.add("invalid");
+                input.reportValidity(); // Show the message immediately
+
+            } else {
+                input.setCustomValidity("");
+                input.classList.remove("invalid");
+            }
+        });
+    });
 }
 
 
 document.addEventListener("DOMContentLoaded", function() {
-
     setupSolver();
     setupDynamicDate();
     setupValidation();
