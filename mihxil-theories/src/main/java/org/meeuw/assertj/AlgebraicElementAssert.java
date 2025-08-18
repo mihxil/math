124a01070ee452495ec5fe61b3e1589a54036e9b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-08-18 10:11:01 +0200

    Added some options to produce better failures messages.

--- mihxil-theories/src/main/java/org/meeuw/assertj/AlgebraicElementAssert.java
+++ mihxil-theories/src/main/java/org/meeuw/assertj/AlgebraicElementAssert.java
@@ -6,6 +6,13 @@ import org.meeuw.math.abstractalgebra.AlgebraicElement;
 
 public class AlgebraicElementAssert<E extends AlgebraicElement<E>> extends AbstractObjectAssert<AlgebraicElementAssert<E>, E> {
 
+    protected boolean includeClassNames = false;
+
+    protected String expectedDescription = "expected";
+
+    protected String valueDescription = "";
+
+
     protected AlgebraicElementAssert(E o) {
         super(o, AlgebraicElementAssert.class);
     }
@@ -13,7 +20,7 @@ public class AlgebraicElementAssert<E extends AlgebraicElement<E>> extends Abstr
     @SuppressWarnings("UnusedReturnValue")
     public AlgebraicElementAssert<E> isEqTo(E expected) {
         if (!actual.eq(expected)) {
-            assertionError(actual + " ≉ " + expected + "(expected)");
+            assertionError("%s %s≉ %s (%s)".formatted(toString(actual), valueDescription.isEmpty() ? "" : " (" + valueDescription + ") ", toString(expected), expectedDescription));
         }
         return myself;
     }
@@ -21,9 +28,37 @@ public class AlgebraicElementAssert<E extends AlgebraicElement<E>> extends Abstr
     @SuppressWarnings("UnusedReturnValue")
     public AlgebraicElementAssert<E> isNotEqTo(E other) {
         if (actual.eq(other)) {
-            assertionError(actual + " ≈ " + other);
+            assertionError("%s %s≈ %s (%s)".formatted(toString(actual), valueDescription.isEmpty() ? "" : " (" + valueDescription + ") ", toString(other), expectedDescription));
         }
         return myself;
+    }
+
+    protected String toString(E element) {
+        return (includeClassNames ? element.getClass().getSimpleName() + " " : "" ) + element.toString();
+    }
+
+    /**
+     * @since 0.19
+     */
+    public AlgebraicElementAssert<E> withIncludeClassNames(boolean b) {
+         includeClassNames= b;
+         return myself;
+    }
+
+    /**
+     * @since 0.19
+     */
+    public AlgebraicElementAssert<E> withExpectedDescription(String s) {
+         expectedDescription = s;
+         return myself;
+    }
+
+    /**
+     * @since 0.19
+     */
+    public AlgebraicElementAssert<E> withValueDescription(String s) {
+         valueDescription = s;
+         return myself;
     }
 
     void assertionError(String fallback) {
