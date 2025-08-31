674237e8ddfb24b3af661babc86fc376a2a614bd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-08-31 23:44:24 +0200

    clean up a bit.

--- docs/cheerpj-test/index.js
+++ docs/cheerpj-test/index.js
@@ -1,8 +1,6 @@
-const settings = {
+await cheerpjInit({
     version: 17
-}
-console.log("init cheerpj", settings);
-await cheerpjInit(settings);
+});
 
 const pref = document.location.pathname.startsWith("/math") ?
     "/app/math/resources/jars/" :
@@ -10,11 +8,9 @@ const pref = document.location.pathname.startsWith("/math") ?
 const version = "0.19-SNAPSHOT"
 const cj = await cheerpjRunLibrary(`${pref}mihxil-math-${version}.jar:${pref}mihxil-math-parser-${version}.jar:${pref}mihxil-algebra-${version}.jar:${pref}mihxil-configuration-${version}.jar:${pref}mihxil-time-${version}.jar:${pref}original-mihxil-demo-${version}.jar:${pref}mihxil-functional-1.14.jar:${pref}big-math-2.3.2.jar`);
 
-
 /* // it may be a hint that this does not work:
 const Impl = await cj['org.meeuw.math.abstractalgebra.klein.KleinGroup']
 const instance = await Impl.INSTANCE;
-
 const sup = await instance.getSupportedOperators();
 const result = await sup.toString()
 */
@@ -23,9 +19,7 @@ const result = await Impl.getSupportedOperators();
 
 const output = document.querySelector("output");
 
-output.style.whiteSpace = 'pre-wrap';
-
-//const b = await instance.getB();
+output.style.whiteSpace = "pre-wrap";
 output.textContent = `
 Operators of KleinGroup: ${result} (should be [OPERATION])
 
