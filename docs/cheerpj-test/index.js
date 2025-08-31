9953e01acf0c94c1c0004baf8d3c667c6c201477
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-08-31 23:18:37 +0200

    demo for cheerpj bug?

--- docs/cheerpj-test/index.js
+++ docs/cheerpj-test/index.js
@@ -10,15 +10,22 @@ const pref = document.location.pathname.startsWith("/math") ?
 const version = "0.19-SNAPSHOT"
 const cj = await cheerpjRunLibrary(`${pref}mihxil-math-${version}.jar:${pref}mihxil-math-parser-${version}.jar:${pref}mihxil-algebra-${version}.jar:${pref}mihxil-configuration-${version}.jar:${pref}mihxil-time-${version}.jar:${pref}original-mihxil-demo-${version}.jar:${pref}mihxil-functional-1.14.jar:${pref}big-math-2.3.2.jar`);
 
-const Impl = await cj['org.meeuw.math.abstractalgebra.klein.KleinGroup']
 
+/* // it may be a hint that this does not work:
+const Impl = await cj['org.meeuw.math.abstractalgebra.klein.KleinGroup']
 const instance = await Impl.INSTANCE;
 
-const output = document.querySelector("output");
 const sup = await instance.getSupportedOperators();
-
 const a = await sup.toString()
+*/
+const Impl = await cj['org.meeuw.cheerpj.CheerpjTest']
+const a = await Impl.getSupportedOperators();
+
+const output = document.querySelector("output");
+
+
+
 //const b = await instance.getB();
 output.textContent = `
-a: ${a} (should be [OPERATION])
+Operators of KleinGroup: ${a} (should be [OPERATION])
 `
