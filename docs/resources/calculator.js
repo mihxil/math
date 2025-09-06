244b1e6898e7432064825d344a39d36806508760
mihxil <mihxil@users.noreply.github.com>
2025-09-06 14:47:14 +0000

    Automated update of documentation https://mihxil.github.io/math/

--- docs/resources/calculator.js
+++ docs/resources/calculator.js
@@ -19,6 +19,7 @@ export class CalculatorClass extends BaseClass {
         const value = input.value;
         input.value = value.slice(0, start) + c + value.slice(end);
         input.setSelectionRange(start + 1, start + 1);
+        input.focus();
     }
 
     async setupForm() {
@@ -65,15 +66,7 @@ export class CalculatorClass extends BaseClass {
                         span.classList.add('element');
                         span.textContent = await elements[j];
                         span.onclick = async e => {
-                            console.log(e.target.textContent);
-                            const input = this.input;
-                            const start = input.selectionStart;
-                            const end = input.selectionEnd;
-                            const value = input.value;
-                            const insertText = e.target.textContent;
-                            input.value = value.slice(0, start) + insertText + value.slice(end);
-                            input.setSelectionRange(start + insertText.length, start + insertText.length);
-                            input.focus();
+                            this.insert(e.target.textContent);
                         };
                         awaitedElements[j] = span;
                     }
@@ -151,11 +144,20 @@ export class CalculatorClass extends BaseClass {
         const operators = fieldInformation.binaryOperators;
         const dd = document.querySelector("#calculator_operators");
         dd.querySelectorAll("dt").forEach(e => {
-            if (! operators.includes(e.textContent.trim())) {
-                e.hidden = true;
+            const symbol = e.textContent.trim();
+            const title = e.nextElementSibling.textContent;
+            if (! e.hasAttribute("original-display")) {
+                e.setAttribute("original-display", window.getComputedStyle(e).display);
+                e.onclick = async e => {
+                    this.insert(e.target.textContent);
+                };
+            }
+            if (! operators.includes(symbol)) {
+                e.style.display = 'none';
                 e.nextElementSibling.hidden = true;
             } else {
-                e.hidden = false;
+                e.title = title;
+                e.style.display = e.getAttribute("original-display");
                 e.nextElementSibling.hidden = false;
             }
         });
