affd6464b2959c37d511dfb4fff027e495bdc684
mihxil <mihxil@users.noreply.github.com>
2025-09-22 18:14:11 +0000

    Automated update of documentation https://mihxil.github.io/math/

--- docs/resources/calculator.js
+++ docs/resources/calculator.js
@@ -76,62 +76,30 @@ export class CalculatorClass extends BaseClass {
     }
 
     async onInView(Calculator){
-
         await super.onInView(Calculator);
-        // using the field information to update the example per field
         if (this.information === null) {
-            this.information = {};
-            const fi = await (await BaseClass.cj)['org.meeuw.math.demo.Calculator$FieldInformation'];
-            const values = await fi.values();
-
-            for (let i = 0; i < values.length; i++) {
-                const value = await values[i];
-                const elements = await BaseClass.awaitedArray(value.getElements());
-                let elementSpans = null;
-                if (elements) {
-                    elementSpans = [];
-                    for (let j = 0; j < elements.length; j++) {
-                        const span = document.createElement('span');
-                        span.classList.add('element');
-                        span.textContent = elements[j];
-                        span.onclick = async e => {
-                            this.insert(e.target.textContent);
-                        };
-                        elementSpans[j] = span;
-                    }
-                }
-                this.information[await values[i].name()] = {
-                    examples: await BaseClass.awaitedArray(value.getExamples()),
-                    elements: elements,
-                    elementsSpans: elementSpans,
-                    binaryOperators: await BaseClass.awaitedArray(value.getBinaryOperators()),
-                    unaryOperators: await BaseClass.awaitedArray(value.getUnaryOperators()),
-                    finite: await value.isFinite(),
-                    description: await value.getDescription(),
-                    help: await value.getHelp()
-                };
-            }
-            console.log(JSON.stringify(this.information));
+            await this.loadInformation();
+            await this.updateFieldList();
+            await this.updateExamples();
+            await this.updateHelp();
+            await this.updateOperators();
+            await this.updateDigits();
         }
-        await this.updateFieldList();
+
         this.field.addEventListener('change', () => {
-            this.updateDataList();
+            this.updateExamples();
             this.updateHelp();
             this.updateOperators();
             this.updateDigits();
 
         });
-        await this.updateDataList();
-        await this.updateHelp();
-        await this.updateOperators();
-        await this.updateDigits();
+
 
 
     }
 
     updateFieldList() {
         for (const [key, value] of Object.entries(this.information)) {
-
             const option = document.createElement('option');
             option.value = key;
             option.text = value.description;
@@ -139,7 +107,53 @@ export class CalculatorClass extends BaseClass {
         }
     }
 
-    async updateDataList() {
+    /**
+     * Load all information about the supported fields
+     */
+    async loadInformation() {
+        this.information = {};
+        const fi = await (await BaseClass.cj)['org.meeuw.math.demo.Calculator$FieldInformation'];
+        const values = await fi.values();
+
+        for (let i = 0; i < values.length; i++) {
+            const value = await values[i];
+            const elements = await BaseClass.awaitedArray(value.getElements());
+            let elementSpans = null;
+            if (elements) {
+                elementSpans = [];
+                for (let j = 0; j < elements.length; j++) {
+                    const span = document.createElement('span');
+                    span.classList.add('element');
+                    span.textContent = elements[j];
+                    span.onclick = async e => {
+                        this.insert(e.target.textContent);
+                    };
+                    elementSpans[j] = span;
+                }
+            }
+            this.information[await values[i].name()] = {
+                examples: await BaseClass.awaitedArray(value.getExamples()),
+                elements: elements,
+                elementsSpans: elementSpans,
+                binaryOperators: await BaseClass.awaitedArray(value.getBinaryOperators()),
+                unaryOperators: await BaseClass.awaitedArray(value.getUnaryOperators()),
+                finite: await value.isFinite(),
+                description: await value.getDescription(),
+                help: await value.getHelp()
+            };
+        }
+        console.log("information", JSON.stringify(this.information,  (key, value) => {
+            if (key === 'elementsSpans') {
+                return undefined;
+            }
+            return value;
+        } , 1));
+    }
+
+    /**
+     *
+     */
+    async updateExamples() {
         const selectedField = this.field.value;
         const information = this.information[selectedField];
         if (information) {
@@ -149,7 +163,7 @@ export class CalculatorClass extends BaseClass {
                 option.value = example;
                 this.inputDataList.appendChild(option);
             }
-            console.log("Updated data list for", selectedField, information.examples);
+            console.log("Updated example li for", selectedField, information.examples);
         }
     }
     async updateHelp() {
@@ -177,24 +191,24 @@ export class CalculatorClass extends BaseClass {
             this.insertOperator(e.target.textContent);
         });
     }
-    elementsDts(dl, operators) {
-        this.dts(dl, operators, async e => {
-            this.insert(e.target.textContent);
-        });
-    }
 
-    dts(dl, operators, onclick) {
+
+    dts(dl, items, onclick, limit=10000) {
         const list = dl.querySelectorAll("dt");
-        const symbolsInList = Array.from(list).map(e => e.textContent.trim());
-        const unmatchedOperators = operators.filter(op => !symbolsInList.includes(op));
-        unmatchedOperators.forEach(op => {
+
+        const currentlyInList = Array.from(list).map(e => e.textContent.trim());
+        const unmatchedItems = items.slice(0, limit).filter(op => !currentlyInList.includes(op));
+
+        // add the ones not yet in it.
+        unmatchedItems.forEach(op => {
             const dt = document.createElement("dt");
-            dt.classList.add('hdlist1');
+            //dt.classList.add('hdlist1');
             dt.textContent = op;
             dl.appendChild(dt);
             const dd = document.createElement("dd");
             dl.appendChild(dd);
         });
+        let count = 0;
         for (const e of dl.querySelectorAll("dt")) {
             const symbol = e.textContent.trim();
             const title = e.nextElementSibling.textContent;
@@ -202,13 +216,17 @@ export class CalculatorClass extends BaseClass {
                 e.setAttribute("original-display", window.getComputedStyle(e).display);
                 e.onclick =onclick;
             }
-            if (!operators.includes(symbol)) {
+            if (!items.includes(symbol)) {
                 e.style.display = 'none';
                 e.nextElementSibling.hidden = true;
             } else {
                 e.title = title;
                 e.style.display = e.getAttribute("original-display");
                 e.nextElementSibling.hidden = false;
+                count++;
+                if (count >= limit) {
+                    break;
+                }
             }
         }
     }
@@ -225,7 +243,10 @@ export class CalculatorClass extends BaseClass {
     async updateDigits() {
         const fieldInformation =  this.information[this.field.value];
         const elements = fieldInformation.elements;
-        this.elementsDts(document.querySelector("#calculator_digits dl"), elements);
+        this.dts(document.querySelector("#calculator_digits dl"), elements, async e => {
+            this.insert(e.target.textContent);
+        }, 10);
+
     }
 
     async onSubmit(Calculator) {
