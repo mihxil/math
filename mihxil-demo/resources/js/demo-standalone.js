bbaf6dbd8e00959c4a6e561f12afa8bd45fafc23
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-11-17 21:16:00 +0100

    Fixed init.

--- mihxil-demo/resources/js/demo-standalone.js
+++ mihxil-demo/resources/js/demo-standalone.js
@@ -1,17 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
+import { BaseClass } from "./base.js";
+
+document.addEventListener('DOMContentLoaded', async () => {
     const tocLinks = Array.from(document.querySelectorAll('#toc a'));
 
     function updateActive() {
         const hash = location.hash || '';
+        let matched = false;
         tocLinks.forEach(a => {
             if (hash && a.getAttribute('href') === hash) {
                 a.classList.add('active');
+                matched = true;
             } else {
                 a.classList.remove('active');
             }
         });
+        if (!matched) {
+            tocLinks[0].classList.add('active');
+            location.hash = tocLinks[0].getAttribute('href');
+        }
     }
 
     window.addEventListener('hashchange', updateActive);
+
     updateActive();
+     const cjj = await BaseClass.getCheerpj();
 });
--- mihxil-demo/resources/js/demo.js
+++ mihxil-demo/resources/js/demo.js
@@ -1,28 +1,7 @@
-import {BaseClass} from "./base.js";
-
 document.addEventListener("DOMContentLoaded", async function() {
     document.querySelectorAll(".demo button").forEach(button => {
         button.setAttribute('data-original-text', button.textContent);
         button.textContent = "waiting";
         button.disabled = true;
     });
-
-    const isDemo = document.querySelector('meta[name="demo"]')?.content;
-    if (isDemo) {
-        console.log("demo page");
-        const cjj = await BaseClass.getCheerpj();
-       /* const ArrayList = await cjj['java.util.ArrayList'];
-        console.log("class", ArrayList);
-        const list = await (new ArrayList());
-        console.log("list", list);
-        await list.add("foo");
-        await list.add("bar");
-        console.log("list", await list.toString());
-        const stream = await list.stream();
-        const i = await stream.iterator();
-        while (await i.hasNext()) {
-            console.log("list item", await i.next());
-        }
-        console.log(list);*/
-    }
 });
