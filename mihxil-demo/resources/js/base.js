e6d79b092be6a99fa582d0c190492d12976ce592
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-11-19 12:31:03 +0100

    smaller scoped again.

--- mihxil-demo/resources/js/base.js
+++ mihxil-demo/resources/js/base.js
@@ -18,11 +18,7 @@ export class BaseClass {
 
 
     static async getCheerpj() {
-         const version = "0.19-SNAPSHOT";
-        const pref = document.location.pathname.startsWith("/math") ?
-                "/app/math/resources/jars/" :
-                "/app/resources/jars/";
-        const resources = `${pref}mihxil-math-${version}.jar:${pref}mihxil-math-parser-${version}.jar:${pref}mihxil-algebra-${version}.jar:${pref}mihxil-configuration-${version}.jar:${pref}mihxil-time-${version}.jar:${pref}original-mihxil-demo-${version}.jar:${pref}mihxil-functional-1.15.jar:${pref}big-math-2.3.2.jar`;
+
         if (BaseClass.cjInit === null) {
 
             BaseClass.cjInit = "locked";
@@ -52,7 +48,11 @@ export class BaseClass {
         }
         await BaseClass.cjInit;
         if (BaseClass.cj === null) {
-
+            const version = "0.19-SNAPSHOT";
+            const pref = document.location.pathname.startsWith("/math") ?
+                "/app/math/resources/jars/" :
+                "/app/resources/jars/";
+            const resources = `${pref}mihxil-math-${version}.jar:${pref}mihxil-math-parser-${version}.jar:${pref}mihxil-algebra-${version}.jar:${pref}mihxil-configuration-${version}.jar:${pref}mihxil-time-${version}.jar:${pref}original-mihxil-demo-${version}.jar:${pref}mihxil-functional-1.15.jar:${pref}big-math-2.3.2.jar`;
             BaseClass.cj = cheerpjRunLibrary(resources);
         }
 
