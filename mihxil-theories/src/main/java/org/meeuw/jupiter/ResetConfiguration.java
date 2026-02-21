ec56629b525694c6799ed26518ca710aa078185d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-02-21 22:10:10 +0100

    Reset configuration.

---
+++ mihxil-theories/src/main/java/org/meeuw/jupiter/ResetConfiguration.java
@@ -0,0 +1,18 @@
+package org.meeuw.jupiter;
+
+import lombok.extern.java.Log;
+
+import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.TestPlan;
+
+import org.meeuw.configuration.ConfigurationService;
+
+@Log
+public class ResetConfiguration implements TestExecutionListener {
+
+    @Override
+    public void testPlanExecutionFinished(TestPlan testPlan) {
+        log.info("Resetting configuration to defaults");
+        ConfigurationService.resetToDefaults();
+    }
+}
