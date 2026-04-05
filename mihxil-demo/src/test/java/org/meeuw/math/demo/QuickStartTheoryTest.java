7163aa12a1ebf83745d5c00015895fba0848e3d5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-04-05 22:59:30 +0200

    More example

--- mihxil-demo/src/test/java/org/meeuw/math/demo/QuickStartTheoryTest.java
+++ mihxil-demo/src/test/java/org/meeuw/math/demo/QuickStartTheoryTest.java
@@ -2,17 +2,28 @@ package org.meeuw.math.demo;
 
 import lombok.extern.java.Log;
 
-import net.jqwik.api.Arbitraries;
-import net.jqwik.api.Arbitrary;
+import net.jqwik.api.*;
+import org.junit.jupiter.api.Test;
 
 import org.meeuw.theories.ComparableTheory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Log
 public class QuickStartTheoryTest implements ComparableTheory<MyComparable>{
 
 
     @Override
+    @Provide
     public Arbitrary<MyComparable> datapoints() {
         return Arbitraries.integers().between(-10, 10).map(MyComparable::new);
+    }
+
+    /**
+     * Other (junit) tests can just be added in the same class too.
+     */
+    @Test
+    public void otherTest() {
+        assertEquals("MyComparable{value=1}", new MyComparable(1).toString());
     }
 }
