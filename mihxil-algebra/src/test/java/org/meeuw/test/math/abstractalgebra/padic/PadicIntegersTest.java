2e3bfe63a8b6a031d1f51eec0bc65b2316ac5627
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-05-30 21:44:52 +0200

    disabled properly

--- mihxil-algebra/src/test/java/org/meeuw/test/math/abstractalgebra/padic/PadicIntegersTest.java
+++ mihxil-algebra/src/test/java/org/meeuw/test/math/abstractalgebra/padic/PadicIntegersTest.java
@@ -7,8 +7,9 @@ import java.util.concurrent.TimeUnit;
 
 import net.jqwik.api.Arbitraries;
 import net.jqwik.api.Arbitrary;
-import org.junit.jupiter.api.*;
-
+import net.jqwik.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
