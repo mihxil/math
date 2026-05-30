5cbf8dd764eea93e9c496d4f75af2770a979e1ee
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-05-30 14:34:43 +0200

    javadoc

--- mihxil-math/src/main/java/org/meeuw/math/uncertainnumbers/CompareConfiguration.java
+++ mihxil-math/src/main/java/org/meeuw/math/uncertainnumbers/CompareConfiguration.java
@@ -7,10 +7,17 @@ import java.util.List;
 
 import org.meeuw.configuration.ConfigurationAspect;
 import org.meeuw.configuration.ConfigurationService;
+import org.meeuw.math.abstractalgebra.AlgebraicElement;
 
+/**
+ * A configuration aspects related to comparing {@link Uncertain uncertain} numbers.
+ */
 @ToString
 public class CompareConfiguration implements ConfigurationAspect {
 
+    /**
+     * Whether {@link Object#equals} must be {@link Uncertain#strictlyEquals(Object) strict}, i.e. preserve transitivity, or {@link org.meeuw.math.abstractalgebra.AbstractAlgebraicElement#eq(AlgebraicElement) not} (allowing for some 'uncertainty' in the equals,  but breaking transitivity).
+     */
     @With
     @Getter
     private boolean equalsIsStrict;
