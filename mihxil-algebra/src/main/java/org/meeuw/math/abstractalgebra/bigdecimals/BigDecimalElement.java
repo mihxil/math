7e41ea752118fe6e73c583043db2cdfd76723a44
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-09-11 23:40:40 +0200

    Replaced `toString` implementation in `BigDecimalElement` to use `FormatService`.

--- mihxil-algebra/src/main/java/org/meeuw/math/abstractalgebra/bigdecimals/BigDecimalElement.java
+++ mihxil-algebra/src/main/java/org/meeuw/math/abstractalgebra/bigdecimals/BigDecimalElement.java
@@ -26,6 +26,7 @@ import org.meeuw.math.exceptions.*;
 import org.meeuw.math.numbers.BigDecimalOperations;
 import org.meeuw.math.numbers.MathContextConfiguration;
 import org.meeuw.math.operators.BasicAlgebraicIntOperator;
+import org.meeuw.math.text.FormatService;
 import org.meeuw.math.text.configuration.UncertaintyConfiguration;
 import org.meeuw.math.uncertainnumbers.*;
 import org.meeuw.math.validation.NotZero;
@@ -413,8 +414,7 @@ public class BigDecimalElement implements
 
     @Override
     public String toString() {
-        // TODO
-        return value.toString();
+        return FormatService.toString(this);
     }
 
 }
